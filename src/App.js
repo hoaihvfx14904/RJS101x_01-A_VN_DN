1b9828c91a6430dc6e5e3e6be7059203b2450f00
hoaihvfx14904 <96828188+hoaihvfx14904@users.noreply.github.com>
2022-01-25 14:59:55 +0700

    Lab03_2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
+import React, { Component} from 'react';
+import {DISHES} from './shared/dishes.js';
 import Menu from './components/MenuComponents.js'
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    }
+  }
+  render(){
   return (
     <div className='App'>
       <Navbar dark color='primary'>
@@ -11,10 +22,11 @@ function App() {
           <NavbarBrand href='/'>Ristorante on Confusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu/>
+      <Menu dishes={this.state.dishes}/>
      
     </div>
   );
+  }
 }
 
 export default App;

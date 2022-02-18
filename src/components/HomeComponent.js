37cd7efa6466a1f84766544b5a99f6b921e4367a
hoaihvfx14904 <96828188+hoaihvfx14904@users.noreply.github.com>
2022-02-18 17:18:10 +0700

    lab10_03

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -33,10 +33,10 @@ function Home (props) {
         <div className="container">
             <div className="row align-items-start">
                 <div className="col-12 col-md m-1">
-                <RenderCard item={props.promotion} isLoading={props.promoLoading} errMess={props.promoErrMess} />
+                <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.promotion} />
+                <RenderCard item={props.promotion} isLoading={props.promoLoading} errMess={props.promoErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
                     <RenderCard item={props.leader} />
